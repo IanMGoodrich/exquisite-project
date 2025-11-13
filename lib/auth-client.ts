1b29b0fa5699ddb01828d1e77a44cd1745bc3aa4
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2025-11-13 11:44:19 -0800

    feat(auth): Update auth-client with new exports and urls.

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.PROD_AUTH_URL || process.env.DEV_AUTH_URL || "http://localhost:3000",
 });
 
-export const { signIn, signUp, useSession } = createAuthClient();
+export const { signIn, signUp, resetPassword, useSession, signOut, forgetPassword } = createAuthClient();
