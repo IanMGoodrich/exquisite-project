d7fb46f02f83a1e7f3bfe69a2b30a926d617ed1a
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2025-11-17 15:11:17 -0800

    feat(Home); Update auth form and container styles.

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,18 @@ import Button from "./components/button/button";
 
 export default async function Home() {
   return (
-    <div>
-      <Button el="link" href="/login">
-        Login
-      </Button>
-      <Button el="link" href="/signup">
-        Sign Up
-      </Button>
-      <Button el="link" href="/dashboard">
-        Dashboard
-      </Button>
+    <div className="container page home">
+      <div className="page form auth">
+        <Button el="link" href="/login">
+          Login
+        </Button>
+        <Button el="link" href="/signup">
+          Sign Up
+        </Button>
+        <Button el="link" href="/dashboard">
+          Dashboard
+        </Button>
+      </div>
     </div>
   );
 }
