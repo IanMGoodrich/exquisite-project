071d47a5664c2efb3cee24d8ac06bf8d9fd1c3a6
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2025-11-17 15:08:42 -0800

    feat(auth): Add better-auth userName plugin.

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,19 @@
 import { createAuthClient } from "better-auth/react";
+import { usernameClient } from "better-auth/client/plugins";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.PROD_AUTH_URL || process.env.DEV_AUTH_URL || "http://localhost:3000",
+  baseURL:
+    process.env.PROD_AUTH_URL ||
+    process.env.DEV_AUTH_URL ||
+    "http://localhost:3000",
+  plugins: [usernameClient()],
 });
 
-export const { signIn, signUp, resetPassword, useSession, signOut, forgetPassword } = createAuthClient();
+export const {
+  signIn,
+  signUp,
+  resetPassword,
+  useSession,
+  signOut,
+  forgetPassword,
+} = createAuthClient();
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,13 +2,17 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 import { PrismaClient } from "../app/generated/prisma/client";
+import { username } from "better-auth/plugins";
 
 const prisma = new PrismaClient();
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "postgresql", // or "mysql", "postgresql", ...etc
+    provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [username()],
+  secret: process.env.BETTER_AUTH_SECRET,
 });
