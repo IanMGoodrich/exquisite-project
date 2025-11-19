e7c7c55ed224e5c016821df373c4f7348ead9662
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2025-11-19 14:24:49 -0800

    feat(login page): Add login functionality.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,24 +1,73 @@
 "use client";
 import { useState } from "react";
-import Form from "../components/form/form";
 import Input from "../components/input/input";
 import Button from "../components/button/button";
+import { useRouter } from "next/navigation";
+import { signIn } from "../../lib/auth-client";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
-  
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError("");
+
+      const result = await signIn.email({
+        email,
+        password,
+      });
+
+      if (result.error) {
+        setError(result?.error?.message || "Login failed");
+        setLoading(false);
+        return;
+      }
+
+      router.push("/");
+    } catch (error) {
+      setError(`An unexpected error occurred`);
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  const errorTemplate = () => {
+    return <div className="error-message">{error}</div>;
+  };
+
   return (
     <div>
       <h1>Login Page</h1>
       <div className="page form auth">
-        <Form>
-          <Input type="text" id="userName" label="Username" />
-          <Input type="password" id="password" label="Password" />
-          <Button type="submit" el="button" />
-        </Form>
+        {error && errorTemplate()}
+        <form className="form" onSubmit={handleSubmit}>
+          <Input
+            type="email"
+            id="email"
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            type="password"
+            id="password"
+            label="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" el="button" disabled={loading}>
+            {loading ? "Signing In..." : "Sign In"}
+          </Button>
+        </form>
+        <Button el="link" href="/forgot-password">
+          Forgot Password?
+        </Button>
       </div>
     </div>
   );
