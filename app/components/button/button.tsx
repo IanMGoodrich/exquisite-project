ab4c13633dc7551779754fda2db50065b19bdf9a
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2025-11-19 14:35:33 -0800

    feat(button): Add 'as' prop to handle styling override to element function.

--- app/components/button/button.tsx
+++ app/components/button/button.tsx
@@ -3,24 +3,30 @@
 import { type ComponentPropsWithoutRef, type FC } from "react";
 import "./button.css";
 
+export type ButtonAsInterface = "button" | "link"
+
 type ButtonProps = {
   el: "button";
+  as?: ButtonAsInterface
 } & ComponentPropsWithoutRef<"button">;
 
 
 type LinkProps = {
   el: "link";
+  as?: ButtonAsInterface
 } & ComponentPropsWithoutRef<"a">;  
 
 
 const Button: FC<ButtonProps | LinkProps> = ({...props}) => {
+  const styleAs = props.as? ` as-${props.as}` : `as-${props.el}`;
+
   if (props.el === "link") {
     return (
-      <a className="button as-link" {...props} />
+      <a className={`button ${styleAs}`} {...props} />
     );
   }
   return (
-    <button className="button as-button" {...props} />
+    <button className={`button ${styleAs}`} {...props} />
   );
 }
 
