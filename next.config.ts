1c8fca4ac0727305703e6456307b03dbfcc6e694
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2025-11-25 17:40:42 -0800

    feat(config): Update config to use all images for now,

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   images: {
+    // Configuration options for the Next.js Image component
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*', // Allow images from all hosts for now TODO: Update later.
+      },
+    ],
+    // Other image options like unoptimized, deviceSizes, imageSizes, domains, etc.
+  },
 };
 
 export default nextConfig;
