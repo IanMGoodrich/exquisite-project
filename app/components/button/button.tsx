7a6c5651615586616a01ee586c21d46227d2936f
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-01-08 10:17:33 -0800

    feat(button): Update to use css vars and theme,  optional secondary classes.

--- app/components/button/button.tsx
+++ app/components/button/button.tsx
@@ -7,26 +7,31 @@ export type ButtonAsInterface = "button" | "link"
 
 type ButtonProps = {
   el: "button";
-  as?: ButtonAsInterface
+  as?: ButtonAsInterface;
+  secondary?: boolean;
+  classes?: string;
 } & ComponentPropsWithoutRef<"button">;
 
 
 type LinkProps = {
   el: "link";
-  as?: ButtonAsInterface
+  as?: ButtonAsInterface;
+  secondary?: boolean;
+  classes?: string;
 } & ComponentPropsWithoutRef<"a">;  
 
 
 const Button: FC<ButtonProps | LinkProps> = ({...props}) => {
   const styleAs = props.as? ` as-${props.as}` : `as-${props.el}`;
+  const isSecondary = props.secondary ? "secondary" : "";
 
   if (props.el === "link") {
     return (
-      <a className={`button ${styleAs}`} {...props} />
+      <a className={`button ${styleAs} ${isSecondary} ${props.classes}`} {...props} />
     );
   }
   return (
-    <button className={`button ${styleAs}`} {...props} />
+    <button className={`button ${styleAs} ${isSecondary} ${props.classes}`} {...props} />
   );
 }
 
