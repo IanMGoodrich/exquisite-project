4e406c6b9271a2112d1e3cac710d0c71f74cb6da
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-01-08 10:18:26 -0800

    feat(header): Update component to use dropdown and incorporate theme.

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,16 +1,21 @@
 "use client";
-import Link from 'next/link';
+import Link from "next/link";
+import { useTheme } from "next-themes";
 import Button from "../button/button";
+import Container from "../container/container";
+import Dropdown from "../dropdown/dropdown";
+import { availableThemes } from "@/lib/constants";
 import { getSession, signOut } from "../../../lib/auth-client";
-import { useEffect, useState, startTransition } from "react";
+import React, { useEffect, useState, startTransition } from "react";
 import { usePathname, useRouter } from "next/navigation";
+import { isAvailableTheme } from "../../../lib/constants";
 import "./header.css";
-
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userId, setUserId] = useState<string | null>(null);
   const pathname = usePathname();
   const router = useRouter();
+  const { setTheme, resolvedTheme } = useTheme();
 
   useEffect(() => {
     let mounted = true;
@@ -20,14 +25,14 @@ const Header = () => {
         const { data } = await getSession();
         if (!mounted) return;
 
-        if (data?.session && data?.user) {          
+        if (data?.session && data?.user) {
           startTransition(() => {
             setIsLoggedIn(Boolean(data.session && data.user));
             setUserId(data.user.id);
           });
         } else {
-          startTransition(() => {            
-            return setIsLoggedIn(false)
+          startTransition(() => {
+            return setIsLoggedIn(false);
           });
         }
       } catch (error) {
@@ -44,13 +49,30 @@ const Header = () => {
     };
   }, [pathname, isLoggedIn]);
 
+  const handleOnClick = (e: React.MouseEvent) => {
+    const selected = (e.target as HTMLLIElement)?.getAttribute("data-value");
+    console.log("header clickhandler", e.target, selected);
+    if (selected && isAvailableTheme(selected)) {
+      setTheme(selected);
+    }
+  };
+
+  const handleOnKey = (e: React.KeyboardEvent) => {
+    console.log();
+    if (e.key !== "Enter") return;
+    const selected = (e.target as HTMLLIElement)?.getAttribute("data-value");
+    if (selected && isAvailableTheme(selected)) {
+      setTheme(selected);
+    }
+  };
+
   const handleSignOut = async () => {
     await signOut({
       fetchOptions: {
         onSuccess: () => {
           startTransition(() => {
             setIsLoggedIn(false);
-            router.push("/")
+            router.push("/");
           });
         },
       },
@@ -58,56 +80,136 @@ const Header = () => {
   };
 
   return (
-    <header className="header">
-      <Link href={"/"}>
-        <p className="logo">EXCRP</p>
-      </Link>
-      <nav className="main-nav">
-        <ul>
-          <li>
-            <Button el="link" href="/">
-              Home
-            </Button>
-          </li>
-          {!isLoggedIn && (
-            <>
-            <li>
-              <Button el="link" href="/login">
-                Login
+    <Container classes="header-container">
+      <header className="header">
+        <Link href={"/"}>
+          <p className="logo">EXCRP</p>
+        </Link>
+        <nav className="main-nav">
+          <ul className="main-nav--list desktop">
+            <li className="main-nav--item">
+              <Button el="link" href="/">
+                Home
               </Button>
             </li>
-            <li>
-              <Button el="link" href="/signup">
-                Signup
-              </Button>
-            </li>
-            </>
-            )
-            }
-            {isLoggedIn && (
+            {!isLoggedIn && (
               <>
-              <li>
-                <Button el="link" href={`/${userId}/profile`}>
-                  Profile
-                </Button>
-              </li>
-              <li>
-              <Button
-                el="button"
-                as="link"
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleSignOut();
-                }}
-              >
-                Log Out
-              </Button>
-              </li>
+                <li className="main-nav--item">
+                  <Button el="link" href="/login">
+                    Login
+                  </Button>
+                </li>
+                <li className="main-nav--item">
+                  <Button el="link" href="/signup">
+                    Signup
+                  </Button>
+                </li>
               </>
             )}
-        </ul>
-      </nav>
-    </header>
+            {isLoggedIn && (
+              <>
+                <li className="main-nav--item">
+                  <Button el="link" href={`/${userId}/profile`}>
+                    Profile
+                  </Button>
+                </li>
+                <li className="main-nav--item">
+                  <Button
+                    el="button"
+                    as="link"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleSignOut();
+                    }}
+                  >
+                    Log Out
+                  </Button>
+                </li>
+              </>
+            )}
+            <li className="main-nav--item">
+              <Button el="link" href={`/about`}>
+                About
+              </Button>
+            </li>
+            <li className="main-nav--dropdown">
+              <Dropdown
+                label="Theme"
+                options={[...availableThemes]}
+                onClickHandler={(e: React.MouseEvent) => {
+                  handleOnClick(e);
+                }}
+                onKeyHandler={(e: React.KeyboardEvent) => {
+                  handleOnKey(e);
+                }}
+              />
+            </li>
+          </ul>
+          <ul className="main-nav--list mobile">
+            <Dropdown label="Menu">
+              <li className="main-nav--item">
+                <Button el="link" href="/">
+                  Home
+                </Button>
+              </li>
+              {!isLoggedIn && (
+                <>
+                  <li className="main-nav--item">
+                    <Button el="link" href="/login">
+                      Login
+                    </Button>
+                  </li>
+                  <li className="main-nav--item">
+                    <Button el="link" href="/signup">
+                      Signup
+                    </Button>
+                  </li>
+                </>
+              )}
+              {isLoggedIn && (
+                <>
+                  <li className="main-nav--item">
+                    <Button el="link" href={`/${userId}/profile`}>
+                      Profile
+                    </Button>
+                  </li>
+                  <li className="main-nav--item">
+                    <Button
+                      el="button"
+                      as="link"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleSignOut();
+                      }}
+                    >
+                      Log Out
+                    </Button>
+                  </li>
+                </>
+              )}
+              <li className="main-nav--item">
+                <Button el="link" href={`/about`}>
+                  About
+                </Button>
+              </li>
+              <li className="main-nav--dropdown">
+                <Dropdown
+                  label="Theme"
+                  options={[...availableThemes]}
+                  startOpen={true}
+                  onClickHandler={(e: React.MouseEvent) => {
+                    handleOnClick(e);
+                  }}
+                  onKeyHandler={(e: React.KeyboardEvent) => {
+                    handleOnKey(e);
+                  }}
+                />
+              </li>
+            </Dropdown>
+          </ul>
+        </nav>
+      </header>
+    </Container>
   );
 };
 
