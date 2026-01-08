4f7a679a60dcdc5877e182977c890e7893c645cb
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-01-08 10:20:44 -0800

    feat(userHomePage): Minor updates with added content for styling/testing.

--- app/(pages)/[userId]/page.tsx
+++ app/(pages)/[userId]/page.tsx
@@ -1,9 +1,11 @@
 import prisma from "../../../lib/prisma";
 import { notFound } from "next/navigation";
 import Image from "next/image";
+import Button from "@/app/components/button/button";
 import { auth } from "../../../lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 type Props = {
   params: Promise<{ userId: string }>;
 };
@@ -43,6 +45,7 @@ export default async function UserHomePage({ params }: Props) {
       <div>
         {user.image && (
           <Image
+            loading="eager"
             width={250}
             height={250}
             src={user.image}
@@ -54,11 +57,53 @@ export default async function UserHomePage({ params }: Props) {
         <div>
           {user.stories.length ? (
             user.stories.map((story) => (
-              <div key={story.id}>{story.title} </div>
+              <div key={story.id}>
+                <Link
+                  className="button as-link"
+                  href={`${userId}/stories/${story.id}`}
+                >
+                  {story.title}
+                </Link>
+              </div>
             ))
           ) : (
-            <div>Time to begin a story</div>
+            <p>Time to begin a story</p>
           )}
+          <span style={{ margin: '0.75rem 0', display: 'inline-block' }}>
+            <Button el="link" as="button" href={`${userId}/stories/create`}>
+              Create New Story
+            </Button>
+          </span>
+        </div>
+        <div>
+          <p>
+            Curabitur at felis non libero suscipit fermentum. Duis volutpat,
+            ante et scelerisque luctus, sem nulla placerat leo, at aliquet
+            libero justo id nulla. Integer at dui nec magna posuere fringilla.
+            Nunc euismod bibendum augue. Cras nec ligula velit. Donec in laoreet
+            leo. Sed at risus vel nulla consequat fermentum. Donec et orci
+            mauris. Nullam tempor velit id mi luctus, a scelerisque libero
+            accumsan. In hac habitasse platea dictumst. Cras ac nunc nec massa
+            tristique fringilla. Nam tempor finibus lorem, nec varius arcu
+            convallis sed. Nunc id orci a neque vehicula malesuada. Donec
+            vehicula libero vel leo convallis, nec tincidunt felis tincidunt.
+            Maecenas euismod tristique leo, vel malesuada ligula malesuada sed.
+            Donec eget libero id leo congue venenatis.
+          </p>
+          <p>
+            Curabitur at felis non libero suscipit fermentum. Duis volutpat,
+            ante et scelerisque luctus, sem nulla placerat leo, at aliquet
+            libero justo id nulla. Integer at dui nec magna posuere fringilla.
+            Nunc euismod bibendum augue. Cras nec ligula velit. Donec in laoreet
+            leo. Sed at risus vel nulla consequat fermentum. Donec et orci
+            mauris. Nullam tempor velit id mi luctus, a scelerisque libero
+            accumsan. In hac habitasse platea dictumst. Cras ac nunc nec massa
+            tristique fringilla. Nam tempor finibus lorem, nec varius arcu
+            convallis sed. Nunc id orci a neque vehicula malesuada. Donec
+            vehicula libero vel leo convallis, nec tincidunt felis tincidunt.
+            Maecenas euismod tristique leo, vel malesuada ligula malesuada sed.
+            Donec eget libero id leo congue venenatis.
+          </p>
         </div>
       </div>
     </div>
