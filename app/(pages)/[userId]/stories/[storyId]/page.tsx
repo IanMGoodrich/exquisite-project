5ef1beb9289dbbb03693b1f4547fb95a49f67042
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-01-30 15:46:06 -0800

    feat(storyPage): Add StoryDisplay component, update story fetch logic to include likedBy array.

--- app/(pages)/[userId]/stories/[storyId]/page.tsx
+++ app/(pages)/[userId]/stories/[storyId]/page.tsx
@@ -1,6 +1,6 @@
 import prisma from "../../../../../lib/prisma";
 import Link from "next/link";
-
+import StoryDisplay from "@/app/components/storyDisplay/storyDisplay";
 type Props = {
   params: Promise<{ userId: string; storyId: string }>;
 };
@@ -13,8 +13,21 @@ export default async function StoryPage({ params }: Props) {
     where: { id: storyId },
     select: {
       title: true,
-      content: true,
+      id: true,
+      content: {
+        select: {
+          id: true,
+          content: true,
+          authorId: true,
+          storyId: true,
+          createdAt: true,
+          reveal: true,
+          likedBy: { select: { userId: true } },
+        },
+      },
       contributors: true,
+      createdAt: true,
+      createdById: true,
       completed: true,
       rounds: true,
       completedAt: true,
@@ -22,7 +35,12 @@ export default async function StoryPage({ params }: Props) {
       nextContributorId: true,
     },
   });
-  if (story && story.nextContributorId && story.nextContributorId !== userId) {
+  if (
+    story &&
+    !story.completed &&
+    story.nextContributorId &&
+    story.nextContributorId !== userId
+  ) {
     const nextUser = await prisma.user.findUnique({
       where: { id: story.nextContributorId },
       select: {
@@ -31,15 +49,20 @@ export default async function StoryPage({ params }: Props) {
     });
     nextUserName = typeof nextUser === "string" ? nextUser : "the next person";
   }
+  const isCreator = userId === story?.createdById;
 
   const inProgressTemplate = () => {
     return (
       <div className="in-progress--wrapper">
+        <div>
+          <h1>{story?.title}</h1>
+        </div>
         <h2>This story in still a work in progress</h2>
         <p>
           Round:
           <span>
-            {story?.completedRounds? story.completedRounds : 0}/{story?.rounds.toString()}
+            {story?.completedRounds ? story.completedRounds : 0}/
+            {story?.rounds.toString()}
           </span>
         </p>
         {story?.nextContributorId === userId ? (
@@ -51,22 +74,15 @@ export default async function StoryPage({ params }: Props) {
     );
   };
 
-  const completedStoryTemplate = () => {
-    return (
-      <div className="completed--wrapper">
-        {story?.content &&
-          story.content.length > 0 &&
-          story.content.map((segment) => (
-            <p key={segment.id}>{segment.content}</p>
-          ))}
-      </div>
-    );
-  };
+  console.log("Story page story data", story);
 
   return (
     <main style={{ maxWidth: 640, margin: "0 auto", padding: "1rem" }}>
-      <h1>{story?.title}</h1>
-      {story?.completed ? completedStoryTemplate() : inProgressTemplate()}
+      {story && story?.completed ? (
+        <StoryDisplay userId={userId} storyData={story} isCreator={isCreator} />
+      ) : (
+        inProgressTemplate()
+      )}
     </main>
   );
 }
