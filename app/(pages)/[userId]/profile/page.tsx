51ea84d8c61cf1b17b119b482fef3f6c21ea3e17
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-01-30 15:44:31 -0800

    feat(User Profile Update): Add new fetch method, ImageWrapper component.

--- app/(pages)/[userId]/profile/page.tsx
+++ app/(pages)/[userId]/profile/page.tsx
@@ -1,62 +1,32 @@
-import prisma from "../../../../lib/prisma";
-import { notFound } from "next/navigation";
+import { getAuthenticatedUser } from "../../../../lib/auth-utils";
 import ProfileForm from "../../../components/profileform/profileform";
-import Image from "next/image";
-import { auth } from '../../../../lib/auth';
-import { headers } from 'next/headers';
-import { redirect  } from 'next/navigation'
+import ImageWrapper from "@/app/components/image/image";
 
 type Props = {
-  params: Promise<{ userId: string; }>
+  params: Promise<{ userId: string }>;
 };
 
-
-export default async function UserProfilePage({ params }: Props) {
+export default async function UserProfileUpdatePage({ params }: Props) {
   const { userId } = await params;
-
-  const user = await prisma.user.findUnique({
-    where: { id: userId },
-    select: {
-      id: true,
-      email: true,
-      nameFirst: true,
-      nameLast: true,
-      userName: true,
-      phone: true,
-      image: true,
-    },
-  });
-  
-  if (!user) {
-    notFound();
-  } else {
-    // verify session/user
-     const session = await auth.api.getSession({
-          headers: await headers()
-        })
-    
-        if (!session || !session.user || session.user.id !== user.id) {
-          redirect('/')
-        }
-  }
-
+  const user = await getAuthenticatedUser(userId);
 
   return (
-    <div className="profile-page">
+    <div className="profile-update-page">
       <h1>{user.userName ?? `${user.nameFirst} ${user.nameLast}`}</h1>
-      <div className="profile-page--main-content">
-        <div>
-          <ProfileForm variant="update" user={user} />
-        </div>
-        <div>
+      <div className="profile-update-page--main-content">
+        <div className="profile-image--wrapper">
           {user.image && (
-            <Image
+            <ImageWrapper
+              variant="circle"
               width={250}
               height={250}
               src={user.image}
               alt={`image for ${user.userName}`}
             />
           )}
+        </div>
+        <div className="profile-form--wrapper">
+          <ProfileForm variant="update" user={user} />
         </div>
       </div>
     </div>
