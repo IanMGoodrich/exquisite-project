296573b9c0cbfb8d6bf0cd5791e70196e7b9e439
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-02-09 12:47:17 -0800

    feat(vectors):L Add edit pen svg.

--- public/vectors.ts
+++ public/vectors.ts
@@ -2,5 +2,8 @@
 export default {
   'heart' : `<svg xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 241.597 220.057">
   <path d="M243.452 1708.979c-26.934.23-51.58 21.476-55.081 48.293-3.114 23.844 7.33 47.4 23.968 64.215 27.515 27.805 61.227 49.794 83.447 82.547 4.39-4.689 9.278-12.066 14.227-17.529 25.23-27.85 58.166-48.013 80.865-78.155 17.175-22.806 19.103-58.113-.538-80.405-18.25-20.712-51.76-25.17-74.37-9.254-8.226 5.791-15.274 13.37-19.932 22.312-10.053-19.32-30.534-32.214-52.586-32.024z" style="opacity:.98000004;fill:var(--svg-fill, transparent);stroke:var(--svg-border, var(--button-border-primary));stroke-width:5;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:none;stroke-dashoffset:0" transform="translate(-175.323 -1696.476)"/>
+</svg>`,
+'pen': `<svg xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 19.05 19.05">
+  <path style="fill:var(--svg-fill, transparent);stroke:var(--svg-border, var(--button-border-primary));stroke-width=5" d="M14.766 1.917 13.54 3.142a.168.168 90 0 0 0 .238l2.192 2.192a.1.1 180 0 0 .141 0L17.1 4.347a.168.168 90 0 0 0-.238l-2.192-2.192a.1.1 180 0 0-.141 0zm-2.03 2.029L2.222 14.462a.367.367 115.202 0 0-.106.224l-.21 2.23a.224.224 45 0 0 .244.244l2.198-.208a.452.452 154.8 0 0 .277-.13L15.115 6.33a.19.19 90 0 0 0-.27L13 3.947a.186.186 180 0 0-.263 0z"/>
 </svg>`
 }
