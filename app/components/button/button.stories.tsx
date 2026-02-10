e69fd3c988e7e6deec17485bfd54e41e15d9b06d
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-02-09 17:08:35 -0800

    feat(button): Update stories to match changes.

--- app/components/button/button.stories.tsx
+++ app/components/button/button.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Button from "./button";
+import svgs from '../../../public/vectors';
 import {
   Description,
   Title,
@@ -18,8 +19,26 @@ const meta: Meta<typeof Button> = {
       table: {
         type: { summary: "'button' | 'link'" },
         defaultValue: { summary: "'button'" },
+      }
+    },
+    as: {
+      control: { type: "select" },
+      options: ["button", "link"],
+      description: "The appearance style to render the element as: 'button' or 'link'.",
+      table: {
+        type: { summary: "'button' | 'link'" },
+        defaultValue: { summary: "'button'" },
       },
     },
+    svg:{
+      control: { type: "select" },
+      options: Object.keys(svgs),
+      description: "Name of the SVG icon to render inside the button, if any.",
+      table: {  
+        type: { summary: "string" },
+        defaultValue: { summary: "undefined" },
+      },
+    }
   },
   parameters: {
     docs: {
@@ -42,14 +61,13 @@ type Story = StoryObj<typeof Button>;
 export const ButtonElement: Story = {
   args: {
     el: "button",
+    as: "button",
     onClick: () => alert("Button clicked!"),
     children: "Click Me",
   },
   decorators: [
     (Story) => (
-      // <div style={{width: '300px', height: 'auto'}}>
       <Story />
-      // </div>
     ),
   ],
 };
@@ -57,11 +75,8 @@ export const LinkElement: Story = {
   args: {
     el: "link",
     href: "#",
+    as: "link",
     children: "Click Me",
   },
-  decorators: [
-    (Story) => (
-      <Story />
-    ),
-  ],
+  decorators: [(Story) => <Story />],
 };
