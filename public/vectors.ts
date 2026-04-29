a0601d34378aec7cad95181f61b341bb61fe81af
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-04-29 14:00:33 -0700

    feat(vectors): Add svgs.

--- public/vectors.ts
+++ public/vectors.ts
@@ -5,5 +5,18 @@ export default {
 </svg>`,
 'pen': `<svg xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 19.05 19.05">
   <path style="fill:var(--svg-fill, transparent);stroke:var(--svg-border, var(--button-border-primary));stroke-width=5" d="M14.766 1.917 13.54 3.142a.168.168 90 0 0 0 .238l2.192 2.192a.1.1 180 0 0 .141 0L17.1 4.347a.168.168 90 0 0 0-.238l-2.192-2.192a.1.1 180 0 0-.141 0zm-2.03 2.029L2.222 14.462a.367.367 115.202 0 0-.106.224l-.21 2.23a.224.224 45 0 0 .244.244l2.198-.208a.452.452 154.8 0 0 .277-.13L15.115 6.33a.19.19 90 0 0 0-.27L13 3.947a.186.186 180 0 0-.263 0z"/>
-</svg>`
+</svg>`,
+'reply': `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+  <path style="fill:var(--svg-fill, transparent);stroke:var(--svg-border, var(--button-border-primary));stroke-width=5" d="M9.704 8.706A1 1 0 1 0 8.29 7.292l-4.997 5.004a1 1 0 0 0 0 1.413l4.997 4.998a1 1 0 1 0 1.415-1.414L6.41 14H13a8 8 0 0 0 7.996-7.75L21 6a1 1 0 1 0-2 0 6 6 0 0 1-5.775 5.996L13 12H6.414z"/>
+  </svg>`,
+'chevron-up': `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+  <path style="fill:var(--svg-fill, transparent);stroke:var(--svg-border, var(--button-border-primary));stroke-width=5" d="M12 7a1 1 0 0 1 .707.293l7 7a1 1 0 0 1-1.414 1.414L12 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414l7-7A1 1 0 0 1 12 7" clip-rule="evenodd"/>
+  </svg>`,
+'chevron-down': `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+  <path style="fill:var(--svg-fill, transparent);stroke:var(--svg-border, var(--button-border-primary));stroke-width=5" d="M4.293 8.293a1 1 0 0 1 1.414 0L12 14.586l6.293-6.293a1 1 0 1 1 1.414 1.414l-7 7a1 1 0 0 1-1.414 0l-7-7a1 1 0 0 1 0-1.414" clip-rule="evenodd"/>
+  </svg>`,
+'close':`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+  <path style="fill:var(--svg-fill, transparent);stroke:var(--svg-border, var(--button-border-primary));stroke-width=5" d="M10.03 8.97a.75.75 0 0 0-1.06 1.06L10.94 12l-1.97 1.97a.75.75 0 1 0 1.06 1.06L12 13.06l1.97 1.97a.75.75 0 0 0 1.06-1.06L13.06 12l1.97-1.97a.75.75 0 1 0-1.06-1.06L12 10.94z"/>
+  <path style="fill:var(--svg-fill, transparent);stroke:var(--svg-border, var(--button-border-primary));stroke-width=5" fill-rule="evenodd" d="M12 1.25C6.063 1.25 1.25 6.063 1.25 12S6.063 22.75 12 22.75 22.75 17.937 22.75 12 17.937 1.25 12 1.25M2.75 12a9.25 9.25 0 1 1 18.5 0 9.25 9.25 0 0 1-18.5 0" clip-rule="evenodd"/>
+  </svg>`,
 }
