18d125a125c9f9abe2876345e7ef3782ca23c4b0
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-04-29 14:05:07 -0700

    feat(UserHomePage): Add message system to page.

--- app/(pages)/[userId]/page.tsx
+++ app/(pages)/[userId]/page.tsx
@@ -1,8 +1,8 @@
 import { getAuthenticatedUserWithStories } from "../../../lib/auth-utils";
 import ImageWrapper from "@/components/image/image";
 import Button from "@/components/button/button";
-import Link from "next/link";
-
+import MessageThread from "@/components/messageThread/messageThread";
+import MessageForm from "@/components/messageForm/messageForm";
 type Props = {
   params: Promise<{ userId: string }>;
 };
@@ -10,11 +10,10 @@ type Props = {
 export default async function UserHomePage({ params }: Props) {
   const { userId } = await params;
   const user = await getAuthenticatedUserWithStories(userId);
-
   return (
     <div className="profile-homepage">
       <h1>Hello {user.userName}!</h1>
-      <div>
+      <div className="profile-homepage--upper-content">
         {user.image && (
           <ImageWrapper
             loading="lazy"
@@ -27,52 +26,81 @@ export default async function UserHomePage({ params }: Props) {
             blurDataURL="../../../public/images/placeholder.webp"
           />
         )}
+
+
+        <div className="profile-homepage--messages">
+          <h2>Conversations</h2>
+    
+          <MessageForm userId={userId} />
+          {user.messageThreads.length > 0 ? (
+            user.messageThreads.map((thread) => (
+              <MessageThread
+                key={thread.threadId}
+                threadId={thread.threadId}
+                participants={thread.participants}
+                subject={thread.subject}
+                pinned={thread.pinned}
+                messages={thread.messages}
+                createdAt={thread.createdAt}
+                userId={userId}
+              />
+            ))
+          ) : (
+            <p>No messages yet. Start a conversation!</p>
+          )}
+        </div>
       </div>
       <div className="profile-homepage--main-content">
         <div className="profile-homepage--stories-wrapper">
           {user.stories.length ? (
             <>
-            <div>
-              <span className="label">Completed stories</span>
-              <ul className="profile-homepage--stories-list completed">
-                {user.stories.filter((story) => story.completed).map((story) => (
-                  <li key={story.id}>
-                    <Link
-                      className="button as-link"
-                      href={`${userId}/stories/${story.id}`}
-                      >
-                      {story.title}
-                    </Link>
-                  </li>
-                ))
-              }
-              </ul>
-            </div>
-            <div>
-            <span className="label">Stories in progress</span>
-              <ul className="profile-homepage--stories-list in-progress">
-              {user.stories.filter((story) => !story.completed).map((story) => (
-                <li key={story.id}>
-                  <Link
-                    className="button as-link"
-                    href={`${userId}/stories/${story.id}`}
-                  >
-                    {story.title}
-                  </Link>
-                </li>
-              ))
-            }
-            </ul>
-            </div>
+              <div>
+                <span className="label">Completed stories</span>
+                <ul className="profile-homepage--stories-list completed">
+                  {user.stories
+                    .filter((story) => story.completed)
+                    .map((story) => (
+                      <li key={story.id}>
+                        <Button
+                          as="link"
+                          el="link"
+                          className="button as-link"
+                          href={`${userId}/stories/${story.id}`}
+                        >
+                          {story.title}
+                        </Button>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+              <div>
+                <span className="label">Stories in progress</span>
+                <ul className="profile-homepage--stories-list in-progress">
+                  {user.stories
+                    .filter((story) => !story.completed)
+                    .map((story) => (
+                      <li key={story.id}>
+                        <Button
+                          as="link"
+                          el="link"
+                          className="button as-link"
+                          href={`${userId}/stories/${story.id}`}
+                        >
+                          {story.title}
+                        </Button>
+                      </li>
+                    ))}
+                </ul>
+              </div>
             </>
           ) : (
             <p>Time to begin a story</p>
           )}
         </div>
         <div>
-            <Button el="link" as="button" href={`${userId}/stories/create`}>
-              Create New Story
-            </Button>
+          <Button el="link" as="button" href={`${userId}/stories/create`}>
+            Create New Story
+          </Button>
           <p>
             Curabitur at felis non libero suscipit fermentum. Duis volutpat,
             ante et scelerisque luctus, sem nulla placerat leo, at aliquet
