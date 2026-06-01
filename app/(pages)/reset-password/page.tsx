e079e7f1215df0637b619e07c0e3a738f4310f2c
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-06-01 14:32:59 -0700

    feat(page/reset-password): Clean up.

--- app/(pages)/reset-password/page.tsx
+++ app/(pages)/reset-password/page.tsx
@@ -44,27 +44,6 @@ export default function SignupPage() {
         return;
       }
 
-      //Update user profile with additional fields
-      // const updateResponse = await fetch("/api/auth/reset-password", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({
-      //     email,
-      //     token,
-      //     password
-      //   }),
-      // });
-console.log(data);
-
-      // if (data.status) {
-      //   console.error("Profile update failed");
-      //   setError("Profile update failed");
-      //   setLoading(false);
-      //   return;
-      // }
-
       router.push("/");
     } catch (error) {
       setError(`An unexpected error occurred`);
@@ -78,49 +57,6 @@ console.log(data);
       <h1>Signup Page</h1>
       <div className="page form auth">
         <form onSubmit={handleSubmit}>
-          {/* <Input
-            type="text"
-            id="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            placeholder="Enter First Name"
-            label="First Name"
-            required
-          />
-          <Input
-            type="text"
-            id="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            placeholder="Enter Last Name"
-            label="Last Name"
-            required
-          />
-          <Input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter Email"
-            label="Email"
-            required
-          />
-          <Input
-            type="text"
-            id="userName"
-            label="User Name (optional)"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder="Will default to first name and last initial"
-          />
-          <Input
-            type="tel"
-            id="Phone Number"
-            label="Phone Number (optional)"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            placeholder="If you wish to receive SMS notifications"
-          /> */}
           <Input
             type="password"
             id="password"
