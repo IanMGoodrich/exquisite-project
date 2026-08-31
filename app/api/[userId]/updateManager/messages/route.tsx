3edfd80cd7d68779b510dc5e6cf0318b9cf5e54a
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-08-31 12:17:10 -0700

    feat(message): Add message update functionality.

---
+++ app/api/[userId]/updateManager/messages/route.tsx
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ userId: string }> },
+) {
+  try {
+    const session = await auth.api.getSession({ headers: await headers() });
+    const sessionId = session?.user.id;
+    const { userId } = await params;
+
+    if (!userId) {
+      return NextResponse.json({ error: "Missing userId" }, { status: 400 });
+    }
+
+    if (userId !== sessionId) {
+      return NextResponse.json(
+        { error: "Not Authorized User" },
+        { status: 400 },
+      );
+    }
+
+    const data = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        messageThreads: {
+          where: { hasUnreadMessages: true },
+          select: {
+            threadId: true,
+            hasUnreadMessages: true,
+          },
+        },
+      },
+    });
+
+    if (!data) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+    return NextResponse.json(data.messageThreads ?? []);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to fetch update data" },
+      { status: 500 },
+    );
+  }
+}
