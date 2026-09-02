bf0c5eccb6a341f8ecb06e505afbe4b7f9148e40
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-09-02 11:58:02 -0700

    feat(About): Update About page with initial how to.

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -2,9 +2,42 @@ import React from "react";
 
 export default function AboutPage() {
   return (
-    <div>
-      <h1>About</h1>
-      
+     <div className="about-page">
+      <h1>Welcome to Splendiferous Eructations</h1>
+      <h2>A exquisite corpse game made for the express purpose of making ridiculous shit up with my friends and yours.</h2>
+      <p>
+        As a former dramaturge I want to begin with full history of The Exquisite Corpse, and its predecessor 
+        Consequences, which is technically what this is. But as a current developer, 
+        I don&apos;t have time for that. That&apos;s copy and someone else get&apos;s paid to do that.
+      </p>
+
+      <h3>How to</h3>
+      <p>
+        Traditionally play works thusly: The first player writes three to <em>maybe</em> five lines of a story. 
+        Then they fold the page over, leaving only the last line visible to the next person in line. The next player then
+        continues the story with only that line for context. Blather, wince, repeat. After a preset number of rounds,
+        three to four for the average game, the full story is read and hilarity/awkwardness ensue. 
+      </p>
+      <p>
+        Splendiferous Eructations works the same way. On your homepage, scroll down and click the Create New Story button. 
+        On the new story creation screen, set the number of rounds, add players by typing their usernames 
+        (real names may be included later), and add a title if you want to. The ability to change titles after the story is complete
+        is one of the many features on the way.
+      </p>
+      <p>
+        Each round you will write your little heart out, using what context you&apos;ve been given by the previous player, 
+        and things you&apos;ve written in previous rounds. Then you will highlight the last line, or final few words as you see fit, 
+        and click the &quot;Select and Create Reveal&quot; button. Verify that you&apos;re sharing the text that you want to, and hit Submit. Currently,
+        if you need to redo the revealed text, you&apos;ll want to select and copy <em>all</em> of your text and then reload the page. 
+        Fixing how you edit revealed text is another feature update in the works.
+      </p>
+      <p>
+        That&apos;s about it. More features are coming. Feedback is great. There is also a chat/message system, and the ability to 
+        &quot;like&quot; segments of completed stories to give friends props for a particularly good section.
+      </p>
+      <p>&nbsp;</p>
+      <p>Good luck, have fun, surprise each other,</p>
+      <p><strong>Ian</strong></p>
     </div>
   );
 }
