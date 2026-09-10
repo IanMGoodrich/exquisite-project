fb1d3de8ef369e6605b0e0c4e3763cdc1edce705
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-09-10 15:05:02 -0700

    feat(layout): Update head with meta tag to handle bleed through on monitor at mobile.

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,6 +96,7 @@ export default async function RootLayout({
       className={`${specialEliteFont.variable} ${bespokeFont.variable} ${manropeFont.variable} ${melodramaFont.variable} ${satoshiFont.variable} ${tekoFont.variable} ${nippoFont.variable}`}
     >
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
         <ThemeScript />
       </head>
       <body>
