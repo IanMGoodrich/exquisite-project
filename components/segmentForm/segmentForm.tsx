18a4341d93ee3704c77dd4839288eb6ff56e9ba0
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2026-09-11 10:27:49 -0700

    feat(segmentForm): Update segment creation UX.

--- components/segmentForm/segmentForm.tsx
+++ components/segmentForm/segmentForm.tsx
@@ -3,23 +3,30 @@ import { useState, useRef, type FC } from "react";
 import { useRouter } from "next/navigation";
 import Input from "../input/input";
 import Button from "../button/button";
+import "./segmentForm.css";
 type SegmentFormProps = {
   userId: string;
   storyId: string;
   promptText?: string;
-  isLast?:boolean;
+  isLast?: boolean;
 };
 
 /** Segment component */
-const SegmentForm: FC<SegmentFormProps> = ({ userId, storyId, promptText, isLast }) => {
+const SegmentForm: FC<SegmentFormProps> = ({
+  userId,
+  storyId,
+  promptText,
+  isLast,
+}) => {
   const router = useRouter();
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
   const [content, setContent] = useState("");
   const [reveal, setReveal] = useState("");
   const textareaRef = useRef(null);
+  const detailRef= useRef(null);
   const [loading, setLoading] = useState(false);
-
+  const [firstPass, setFirstPass] = useState(true);
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setLoading(true);
@@ -31,7 +38,7 @@ const SegmentForm: FC<SegmentFormProps> = ({ userId, storyId, promptText, isLast
           content,
           authorId: userId,
           storyId,
-          reveal: !isLast ? reveal : 'SKIP_REVEAL_REQUIREMENT_FINAL_SEGMENT',
+          reveal: !isLast ? reveal : "SKIP_REVEAL_REQUIREMENT_FINAL_SEGMENT",
           promptText,
         }),
       });
@@ -56,8 +63,13 @@ const SegmentForm: FC<SegmentFormProps> = ({ userId, storyId, promptText, isLast
     if (textareaRef.current) {
       const { selectionStart, selectionEnd } = textareaRef.current;
       const highlightedText = content.substring(selectionStart, selectionEnd);
-      if (highlightedText && highlightedText.length > 0) {
+      const detailElement= detailRef.current? detailRef.current as HTMLDetailsElement : null;
+      if (highlightedText && highlightedText.trim().length > 0) {
         setReveal(highlightedText);
+        setFirstPass(false);
+        if (detailElement) {
+          detailElement.removeAttribute('open');
+        }
       }
     }
   };
@@ -68,20 +80,28 @@ const SegmentForm: FC<SegmentFormProps> = ({ userId, storyId, promptText, isLast
         {reveal && reveal.length > 0 && (
           <div className="reveal-wrapper">
             <span>This is what the player will see:</span>
-            <p>{reveal}</p>
+            <p>{`"${reveal}..."`}</p>
           </div>
         )}
-        <form onSubmit={handleSubmit}>
-          <Input
-            id="content"
-            label="Write your next instalment"
-            type="textarea"
-            rows={10}
-            ref={textareaRef}
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            required
-          />
+        <form className="segment-form--form" onSubmit={handleSubmit}>
+          <details className="segment-form--detail" open ref={detailRef}>
+            <summary
+              className={`button as-button segment-form--summary ${firstPass ? "hidden" : ""}`}
+            >
+              Open to review or change the reveal, or hit submit!
+            </summary>
+
+            <Input
+              id="content"
+              label={firstPass ? "Write your next instalment" : ""}
+              type="textarea"
+              rows={10}
+              ref={textareaRef}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              required
+            />
+          </details>
           <div className="form-buttons">
             {(reveal.length > 0 || isLast) && (
               <Button type="submit" el="button" disabled={loading}>
@@ -95,6 +115,14 @@ const SegmentForm: FC<SegmentFormProps> = ({ userId, storyId, promptText, isLast
                 disabled={content.length === 0}
               >
                 Select and Create Reveal
+              </Button>
+            )}
+            {reveal.length > 0 && !isLast && (
+              <Button
+                onClick={handleCreateReveal}
+                el="button"
+              >
+                Correct Reveal
               </Button>
             )}
           </div>
